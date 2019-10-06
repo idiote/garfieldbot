3cb6cc6c8734b09550f8f60cf97c9429e6dc62f7
cookie-cuttr <55653866+cookie-cuttr@users.noreply.github.com>
2019-10-06 11:18:20 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,25 +27,23 @@ async def on_message(message):
     if message.author == bot.user:
         return
     
-    if message.content[0:5].lower() == "garf":
-        if message.content[5:10].lower() == "daily":
-                print ("works")
+    #main command list
+    if message.content[0:4].lower() == "garf":
+        if message.content[4:9].lower() == "daily":
+                localtime = time.gmtime()
+                if len(str(localtime.tm_mday)) == 1:
+                    day = "0" + str(localtime.tm_mday)
+                else:
+                    day  = localtime.tm_mday
+
+                garfFileName = "https://d1ejxu6vysztl5.cloudfront.net/comics/garfield/2019/"+str(localtime.tm_year)+"-"+str(localtime.tm_mon)+"-"+day+".gif"
+
+                await message.channel.send(garfFileName)
+                print ("A user requested this comic:", garfFileName)
 
     if "lasagna" in message.content.lower() or "lasagne" in message.content.lower():
         response = "did somebody say lasagna?"
-        await message.channel.send(response)
-
-    if message.content.lower() == "garfdaily":
-        localtime = time.gmtime()
-        if len(str(localtime.tm_mday)) == 1:
-            day = "0" + str(localtime.tm_mday)
-        else:
-            day  = localtime.tm_mday
-
-        garfFileName = "https://d1ejxu6vysztl5.cloudfront.net/comics/garfield/2019/"+str(localtime.tm_year)+"-"+str(localtime.tm_mon)+"-"+day+".gif"
-
-        await message.channel.send(garfFileName)
-        print ("A user requested this comic:", garfFileName)
+        await message.channel.send(response)        
     
     if message.content.lower() == "garfrandom":
         daycap = 31
